97a27032c4703b8b9fa749b86476efde6ca14653
Vanina <chachagua.vanina@gmail.com>
2020-03-29 03:57:16 -0300

    Devuelvo config

--- server/config/config.js
+++ server/config/config.js
@@ -1,17 +1,17 @@
-//  Puerto
+// //  Puerto
 process.env.PORT = process.env.PORT || 3000;
 
 // *** Entorno ***//
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
-if (process.env.NODE_ENV === 'dev') {
-    //BD local
-    urlDB = 'mongodb://localhost:27017/cafe';
+// if (process.env.NODE_ENV === 'dev') {
+//     //BD local
+//     urlDB = 'mongodb://localhost:27017/cafe';
 
-} else {
-    //BD nube
-    urlDB = 'mongodb+srv://lunina:grZNskfQL1Bxf1aw@cluster0-qjv8d.mongodb.net/cafe';
-}
+// } else {
+//     //BD nube
+urlDB = 'mongodb+srv://lunina:grZNskfQL1Bxf1aw@cluster0-qjv8d.mongodb.net/cafe';
+// }
 
 process.env.URLDB = urlDB;
