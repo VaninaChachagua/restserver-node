4474873c0e8a954ae00af7900f98e79d6e3c2cc3
Vanina <chachagua.vanina@gmail.com>
2020-03-29 03:34:30 -0300

    config

--- server/config/config.js
+++ server/config/config.js
@@ -5,12 +5,12 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
-// if (process.env.NODE_ENV === 'dev') {
-//     //BD local
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
-//     //BD nube
-urlDB = 'mongodb+srv://lunina:grZNskfQL1Bxf1aw@cluster0-qjv8d.mongodb.net/cafe';
-// }
+if (process.env.NODE_ENV === 'dev') {
+    //BD local
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    //BD nube
+    urlDB = 'mongodb+srv://lunina:grZNskfQL1Bxf1aw@cluster0-qjv8d.mongodb.net/cafe';
+}
 
 process.env.urlDB = urlDB;
